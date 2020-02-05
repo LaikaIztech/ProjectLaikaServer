c4170c5a98d7fa67edd3ebad3fd44a9717da2043
ambertide <egeemirozkan24@gmail.com>
2020-02-06 01:05:16 +0300

    Update main_server.py

--- src/main_server.py
+++ src/main_server.py
@@ -2,10 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def laika_respond():
-    print(request.data)
-    return "Request acquired"
+    print(request.get_data())
+    return f"Recognised {request.headers.get('User-Agent')}"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=8080)
