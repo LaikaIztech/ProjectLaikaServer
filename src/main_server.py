8a761c345351a0547a3745c71baa4dc0eeaa8fb3
ambertide <egeemirozkan24@gmail.com>
2020-02-06 02:38:34 +0300

    Add detection algorithm

--- src/main_server.py
+++ src/main_server.py
@@ -1,11 +1,24 @@
 from flask import Flask, request
+from time import time
+from os import remove
+from vision.dog_counter import count_doggo
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def laika_respond():
-    print(request.get_data())
     return f"Recognised {request.headers.get('User-Agent')}"
+
+@app.route("/detect", methods=["POST"])
+def get_pictures():
+    photo_data = request.files['media']
+    file_path = f"data/{request.headers.get('User-Agent').replace('/', '')}-{time()}.jpeg"
+    photo_data.save(file_path)
+    dog_count = count_doggo(file_path, "data/catface.xml")
+    print(f"Detected dogs: {dog_count}")
+    remove(file_path)
+    return f"Dogs in photo: {dog_count}."
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=8080)
--- src/vision/dog_counter.py
+++ src/vision/dog_counter.py
@@ -1,7 +1,7 @@
 import cv2
 
-def count_doggo(img_path):
-    dog_cascade = cv2.CascadeClassifier(r'..\data\dog.xml')
+def count_doggo(img_path, xml_path):
+    dog_cascade = cv2.CascadeClassifier(xml_path)
     img = cv2.imread(img_path)
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     dogs = dog_cascade.detectMultiScale(grayscale)
